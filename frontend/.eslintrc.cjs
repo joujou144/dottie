2d1ab5e0428923181f7c173784e450719af27623
Aznim Rahman <aznimr@protonmail.com>
2025-04-21 12:09:22 +0100

    modify eslintrc file

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,17 +2,41 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'test-backend-connection.js', '*config.js', '*config.ts'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    "test-backend-connection.js",
+    "*config.js",
+    "*config.ts",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@typescript-eslint", "unused-imports"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
